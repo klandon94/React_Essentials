a668d298ee4bb54282918908675d3db6be9e895a
Kenny Landon <kenny.landon88@gmail.com>
2018-09-15 13:36:07 -0400

    member list project

---
+++ MemberList/src/components/ui/MemberList.js
@@ -0,0 +1,76 @@
+import { Component } from 'react'
+// allows us to connect to api
+import fetch from 'isomorphic-fetch'
+import Member from './Member'
+
+class MemberList extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            members: [],
+            loading: false,
+            administrators: []
+        }
+        this.makeAdmin = this.makeAdmin.bind(this)
+        this.removeAdmin = this.removeAdmin.bind(this)
+    }
+
+    componentDidMount(){
+        this.setState({loading:true})
+        fetch('https://api.randomuser.me/?nat=US&results=12')
+            .then(response => response.json())
+            .then(json => json.results)
+            .then(members => this.setState({
+                members,
+                loading: false
+            }))
+    }
+
+    makeAdmin(email){
+        const administrators = [
+            ...this.state.administrators,
+            email
+        ]
+        this.setState({administrators})
+    }
+
+    removeAdmin(email){
+        const administrators = this.state.administrators.filter(
+            adminEmail => adminEmail !== email
+        )
+        this.setState({administrators})
+    }
+
+    render() {
+        // destructuring
+    	const { members, loading } = this.state
+        return (
+            <div className="member-list">
+                <h1>Society Members</h1>
+                {
+                    (loading) ? 
+                    <span> loading ... </span> :
+                    <span>{members.length} members </span>
+                }
+                {(members.length) ?
+                    members.map(
+                	(member, i) => 
+                        <Member key={i}
+                                admin={this.state.administrators.some(
+                                    adminEmail => adminEmail === member.email
+                                )}
+                                name={member.name.first + ' ' + member.name.last}
+                                email={member.email} 
+                                thumbnail={member.picture.thumbnail}
+                                makeAdmin = {this.makeAdmin}
+                                removeAdmin = {this.removeAdmin} />
+                     ):
+                     <span>Currently 0 members</span>
+                }
+            </div>
+        )
+    }
+}
+
+export default MemberList
